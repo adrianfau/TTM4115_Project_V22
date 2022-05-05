ed5a91b4f104fb2666f233d5590fd5f88524c330
Kingofrock37 <adrianfauske@gmail.com>
2022-05-05 22:40:09 +0200

    small sync

--- pyvoice/homeserver.py
+++ pyvoice/homeserver.py
@@ -2,6 +2,5 @@ import server
 import socket
 
 print("----Starting Voice Server----")
-voiceip = socket.gethostbyname(socket.gethostname())
 voiceport = 10505
 voice_server = server.Server(voiceport)
